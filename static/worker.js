1ef10661cac18476de25fd9a2dcf2975f019818a
sorz <orz@sorz.org>
2018-01-21 02:21:40 +0800

    Popup error message

--- static/worker.js
+++ static/worker.js
@@ -8,9 +8,13 @@ onmessage = async function(e) {
   let wasm = await Rust.asstosrt_wasm;
   console.log("convert " + file.name);
 
-  let ass = reader.readAsText(file);
-  let srt = wasm.assToSrt(ass);
-  let srtFile = new Blob([srt], {type: 'text/srt'});
-  let srtUrl = URL.createObjectURL(srtFile);
-  postMessage(srtUrl);
+  try {
+    let ass = reader.readAsText(file);
+    let srt = wasm.assToSrt(ass);
+    let srtFile = new Blob([srt], {type: 'text/srt'});
+    let srtUrl = URL.createObjectURL(srtFile);
+    postMessage({file: file, srtUrl: srtUrl});
+  } catch (e) {
+    postMessage({file: file, error: e});
+  }
 }
