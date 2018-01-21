78395444a742b84bf00cdaba581b7d9e9100f939
sorz <orz@sorz.org>
2018-01-21 22:12:23 +0800

    Show save link or error message on HTML page

--- static/worker.js
+++ static/worker.js
@@ -4,18 +4,21 @@ importScripts('js/app.js');
 let reader = new FileReaderSync();
 
 onmessage = async function(e) {
-  let file = e.data;
+  let id = e.data.id;
+  let file = e.data.file;
+  let inCharset = e.data.inCharset;
+  let outCharset = e.data.outCharset;
+
   let wasm = await Rust.asstosrt_wasm;
   console.log("convert " + file.name);
 
   try {
     let ass = reader.readAsArrayBuffer(file);
-    console.log(ass);
-    let srt = wasm.assToSrt(ass, null, null);
+    let srt = wasm.assToSrt(ass, inCharset, outCharset);
     console.log(srt);
     let srtUrl = URL.createObjectURL(srt);
-    postMessage({file: file, srtUrl: srtUrl});
+    postMessage({id: id, file: file, srtUrl: srtUrl});
   } catch (e) {
-    postMessage({file: file, error: e});
+    postMessage({id: id, file: file, error: e});
   }
 }
