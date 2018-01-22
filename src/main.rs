ea1a73cc7fa2b8008662e0b492c86dae523161fc
sorz <orz@sorz.org>
2018-01-23 02:43:23 +0800

    Refactor: add try_js macro

--- src/main.rs
+++ src/main.rs
@@ -34,19 +34,26 @@ macro_rules! log {
     };
 }
 
+macro_rules! try_js {
+    ( $e:expr ) => {
+        $e.unwrap_or_else(|e| throw!(format!("{}", e)))
+    };
+    ( $e:expr, $m:expr, err) => {
+        $e.unwrap_or_else(|e| throw!(format!("{}: {}", $m, e)))
+    };
+    ( $e:expr, $m:expr ) => {
+        $e.unwrap_or_else(|| throw!(format!("{}", $m)))
+    };
+}
+
 #[derive(Deserialize, Debug)]
 struct Charset (String);
 
 #[derive(Deserialize, Debug)]
-enum Lines {
-    First, Last, All
-}
+enum Lines { First, Last, All }
 
 #[derive(Deserialize, Debug)]
-enum ChineseConv {
-    None, S2T, T2S,
-}
-
+enum ChineseConv { None, S2T, T2S }
 
 #[derive(Deserialize, Debug)]
 struct Options {
@@ -59,8 +66,7 @@ js_deserializable!(Options);
 
 impl Into<EncodingRef> for Charset {
     fn into(self) -> EncodingRef {
-        encoding_from_whatwg_label(&self.0)
-            .unwrap_or_else(|| throw!("unknown charset name"))
+        try_js!(encoding_from_whatwg_label(&self.0), "unknown charset name")
     }
 }
 
@@ -76,8 +82,7 @@ fn detect_charset(mut s: &[u8]) -> Option<EncodingRef> {
 fn ass_to_srt(ass: ArrayBuffer, opts: Options) -> Value {
     let ass: Vec<u8> = ass.into();
     let in_charset = opts.in_charset.map_or_else(
-        || detect_charset(&ass)
-            .unwrap_or_else(|| throw!("fail to detect ASS charset")),
+        || try_js!(detect_charset(&ass), "fail to detect ASS charset"),
         |l| l.into());
     let out_charset = opts.out_charset.map_or(in_charset, |l| l.into());
     let dict = match opts.chinese_conv {
@@ -94,20 +99,19 @@ fn ass_to_srt(ass: ArrayBuffer, opts: Options) -> Value {
         }.map(|s| dict.map_or(s.into(), |d| d.replace_all(s)))
     };
 
-    let ass = in_charset.decode(&ass, DecoderTrap::Replace)
-        .unwrap_or_else(|e| throw!(format!("fail to decode: {}", e)));
-    let srt = subtitle::ass_to_srt(&ass, true, Some(mapper))
-        .unwrap_or_else(|e| throw!(e));
+    let ass = try_js!(in_charset.decode(&ass, DecoderTrap::Replace),
+        "fail to decode", err);
+    let srt = try_js!(subtitle::ass_to_srt(&ass, true, Some(mapper)));
 
     let mut output = Vec::new();
     // insert BOM for utf-16
     if out_charset.whatwg_name().map_or(false, |n| n.starts_with("utf-16")) {
-        out_charset.encode_to("\u{feff}", EncoderTrap::Strict, &mut output)
-            .unwrap_or_else(|e| log!(format!("fail to insert BOM: {}", e)));
+        try_js!(out_charset.encode_to(
+            "\u{feff}", EncoderTrap::Strict, &mut output));
     }
 
-    out_charset.encode_to(&srt, EncoderTrap::Replace, &mut output)
-        .unwrap_or_else(|e| throw!(format!("fail to encode: {}", e)));
+    try_js!(out_charset.encode_to(&srt, EncoderTrap::Replace, &mut output),
+        "fail to encode", err);
     let output = unsafe { UnsafeTypedArray::new(&output) };
     js! (return new Blob([@{output}], {type: "text/srt"}))
 }
