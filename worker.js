a1541834b59ae112403ca7ea946c231fb26d56cf
sorz <orz@sorz.org>
2018-01-23 15:46:33 +0800

    Sync: split stylesheet & wasm error handle

--- worker.js
+++ worker.js
@@ -7,15 +7,10 @@ onmessage = async function(e) {
   let id = e.data.id;
   let file = e.data.file;
   let opts = e.data.opts;
-
-  let wasm = await Rust.asstosrt_wasm;
-  console.log("convert " + file.name);
-  console.log(opts);
-
   try {
+    let wasm = await Rust.asstosrt_wasm;
     let ass = reader.readAsArrayBuffer(file);
     let srt = wasm.assToSrt(ass, opts);
-    console.log(srt);
     let srtUrl = URL.createObjectURL(srt);
     postMessage({id: id, srtUrl: srtUrl});
   } catch (e) {
